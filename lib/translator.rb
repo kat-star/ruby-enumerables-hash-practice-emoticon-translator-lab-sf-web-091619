33125df727adfe14f3904180402708ee39759f15
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-14 20:50:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,8 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
     emoticon_h[:get_emoticon][emoticon]
   else
     "Sorry, that emoticon was not found"
-  
-  
+  end
 end
 
 def get_english_meaning
